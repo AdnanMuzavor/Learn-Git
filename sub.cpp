ddb3ebf17d115258badef08649c454c7d5ae83d3
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-02-26 16:13:30 +0530

    commit changes by adding sub, sum after being modified not staged

---
+++ sub.cpp
@@ -0,0 +1,11 @@
+#include <iostream>>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "sum of " << a << " - " << b << " = " << a - b << endl;
+    cin >> a >> b;
+    cout << " sum with new values is: " << a << " - " << b << " = " << a - b << endl;
+    return 0;
+}
