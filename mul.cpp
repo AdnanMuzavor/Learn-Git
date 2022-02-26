17b5afaa60ee6851459088dead4c22de2bbe217d
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-02-26 17:00:13 +0530

    added multiply program

---
+++ mul.cpp
@@ -0,0 +1,11 @@
+#include <iostream>>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "mul of " << a << " * " << b << " = " << a * b << endl;
+    cin >> a >> b;
+    cout << " mul with new values is: " << a << " * " << b << " = " << a * b << endl;
+    return 0;
+}
